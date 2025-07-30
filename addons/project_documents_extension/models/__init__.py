f268825a376f849c059385c2f4106b608d0a8579
sabry youssef <vendorah2@gmail.com>
2025-07-30 20:00:10 +0300

    Implement reached checkpoint functionality and workflow improvements in project_documents_extension

--- addons/project_documents_extension/models/__init__.py
+++ addons/project_documents_extension/models/__init__.py
@@ -1,4 +1,6 @@
 from . import project
 from . import product
 from . import product_task_template
-from . import expiration_reminder 
+from . import expiration_reminder
+from . import document_service
+from . import project_partner_fields 
